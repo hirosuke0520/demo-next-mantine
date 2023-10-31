f702d7ffefd1562ded2eafceeb70a504d8ef667f
nishino-hironori <nishino-hironori@dmm.com>
2023-10-31 09:45:38 +0900

    Mantine導入

---
+++ src/app/demo/page.tsx
@@ -0,0 +1,12 @@
+import { Box, Button, Title } from "@mantine/core";
+
+export const Demo = () => {
+  return (
+    <Box p={36}>
+      <Title order={1}>デモページ</Title>
+      <Button color="cyan">ボタン</Button>
+    </Box>
+  );
+};
+
+export default Demo;
